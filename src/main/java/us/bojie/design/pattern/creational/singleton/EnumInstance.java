e3cc672a2671a235c478785dd8bd89114cd5e50f
Bojie Jiang <jiangbojie@gmail.com>
2018-09-09 23:26:33 -0700

    EnumInstance singleton

---
+++ src/main/java/us/bojie/design/pattern/creational/singleton/EnumInstance.java
@@ -0,0 +1,25 @@
+package us.bojie.design.pattern.creational.singleton;
+
+public enum EnumInstance {
+    INSTANCE {
+        protected void printTest() {
+            System.out.println("Printing");
+        }
+    };
+
+    protected abstract void printTest();
+
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public static EnumInstance getInstance() {
+        return INSTANCE;
+    }
+}
--- src/main/java/us/bojie/design/pattern/creational/singleton/Test.java
+++ src/main/java/us/bojie/design/pattern/creational/singleton/Test.java
@@ -1,8 +1,6 @@
 package us.bojie.design.pattern.creational.singleton;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Test {
@@ -18,40 +16,51 @@ public class Test {
 
         // test Serializable
 //        HungrySingleton instance = HungrySingleton.getInstance();
+//        EnumInstance instance = EnumInstance.getInstance();
+//        instance.setData(new Object());
 //        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton_file"));
 //        oos.writeObject(instance);
 //
 //        File file = new File("singleton_file");
 //        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
 //
-//        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+////        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+//        EnumInstance newInstance = (EnumInstance) ois.readObject();
 //
-//        System.out.println(instance);
-//        System.out.println(newInstance);
-//        System.out.println(instance == newInstance);
+//        System.out.println(instance.getData());
+//        System.out.println(newInstance.getData());
+//        System.out.println(instance.getData() == newInstance.getData());
 
 //        Class objectClass = HungrySingleton.class;
 //        Class objectClass = StaticInnerClassSingleton.class;
-        Class objectClass = LazySingletonSynced.class;
-
-        Constructor constructor = objectClass.getDeclaredConstructor();
-        constructor.setAccessible(true);
+//        Class objectClass = LazySingletonSynced.class;
+//        Class objectClass = EnumInstance.class;
+//
+//        Constructor constructor = objectClass.getDeclaredConstructor(String.class, int.class);
+//        constructor.setAccessible(true);
+//        EnumInstance newInstance = (EnumInstance) constructor.newInstance("BOBO", 666);
 
 //        HungrySingleton instance = HungrySingleton.getInstance();
 //        HungrySingleton newInstance = (HungrySingleton) constructor.newInstance();
 //        StaticInnerClassSingleton instance = StaticInnerClassSingleton.getInstance();
 //        StaticInnerClassSingleton newInstance = (StaticInnerClassSingleton) constructor.newInstance();
-        LazySingletonSynced instance = LazySingletonSynced.getInstance();
+//        LazySingletonSynced instance = LazySingletonSynced.getInstance();
 
         // 破坏阻止反射的flag
-        Field flag = instance.getClass().getDeclaredField("flag");
-        flag.setAccessible(true);
-        flag.set(instance, true);
+//        Field flag = instance.getClass().getDeclaredField("flag");
+//        flag.setAccessible(true);
+//        flag.set(instance, true);
+//
+//        LazySingletonSynced newInstance = (LazySingletonSynced) constructor.newInstance();
 
-        LazySingletonSynced newInstance = (LazySingletonSynced) constructor.newInstance();
+//        EnumInstance instance = EnumInstance.getInstance();
+//        EnumInstance newInstance = (EnumInstance) constructor.newInstance();
 
-        System.out.println(instance);
-        System.out.println(newInstance);
-        System.out.println(instance == newInstance);
+//        System.out.println(instance);
+//        System.out.println(newInstance);
+//        System.out.println(instance == newInstance);
+
+        EnumInstance instance = EnumInstance.getInstance();
+        instance.printTest();
     }
 }
